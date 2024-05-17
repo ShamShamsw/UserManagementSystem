cce530b9bf2a8e06ef055531c0fcc0cea4ba4a77
ShamShamsw <jhaseman3@gmail.com>
2024-05-17 00:58:36 -0500

    Update 1 User Management System

---
+++ User Management System.py
@@ -0,0 +1,615 @@
+users = {
+    10001: {
+        "title": "Software Developer",
+        "first_name": "Jacob",
+        "last_name": "Haseman",
+        "email": "jhaseman@pyt.com",
+        "location": "Mukwonago"
+    },
+    10002: {
+        "title": "Intern",
+        "first_name": "Milly",
+        "last_name": "Smith",
+        "email": "msmith@pyt.com",
+        "location": "Mukwonago"
+    },
+    10003: {
+        "title": "Software Developer Intern",
+        "first_name": "Billy",
+        "last_name": "Cat",
+        "email": "bcat@pyt.com",
+        "location": "Mukwonago"
+    },
+    10004: {
+        "title": "Desktop Support Technician",
+        "first_name": "Minnie",
+        "last_name": "Fire",
+        "email": "mfire@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10005: {
+        "title": "HR Manager",
+        "first_name": "Sarah ",
+        "last_name": "Kim ",
+        "email": "skim@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10006: {
+        "title": "IT Helpdesk Technician",
+        "first_name": "Michael",
+        "last_name": "Chen",
+        "email": "mchen@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10007: {
+        "title": "Desk Worker",
+        "first_name": "Emily",
+        "last_name": "Patel",
+        "email": "epatel@pyt.com",
+        "location": "Mukwonago",
+    },
+
+    10008: {
+        "title": "Software Engineer",
+        "first_name": "Ryan",
+        "last_name": "Nguyen",
+        "email": "rnguyen@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10009: {
+        "title": "HR Coordinator",
+        "first_name": "Jessica",
+        "last_name": "Li",
+        "email": "jli@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10010: {
+        "title": "IT Support Specialist",
+        "first_name": "Kevin",
+        "last_name": "Smith",
+        "email": "ksmith@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10011: {
+        "title": "Administrative Assistant",
+        "first_name": "Amanda",
+        "last_name": "Johnson",
+        "email": "ajohnson@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10012: {
+        "title": "dgarcia@pyt.com",
+        "first_name": "Daniel",
+        "last_name": "Garcia",
+        "email": "Software Tester",
+        "location": "Mukwonago"
+    },
+
+    10013: {
+        "title": "HR Assistant",
+        "first_name": "Samantha",
+        "last_name": "Brown",
+        "email": "sbrown@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10014: {
+        "title": "IT Analyst",
+        "first_name": "Andrew",
+        "last_name": "Lee",
+        "email": "alee@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10015: {
+        "title": "Office Manager",
+        "first_name": "Lauren",
+        "last_name": "Taylor",
+        "email": "ltaylor@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10016: {
+        "title": "IT Manager",
+        "first_name": "Stephanie ",
+        "last_name": "Rivera ",
+        "email": "srivera01@pyt.com",
+        "location": "Mukwonago"
+    },
+
+
+    10017: {
+        "title": "Data Entry Clerk",
+        "first_name": "Racheal",
+        "last_name": "Adams",
+        "email": "radams@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10018: {
+        "title": "Software Developer",
+        "first_name": "Tyler",
+        "last_name": "Wilson",
+        "email": "twilson@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10019: {
+        "title": "HR Specialist",
+        "first_name": "Olivia",
+        "last_name": "Rodriguez",
+        "email": "orodriguez@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10020: {
+        "title": "IT Technician",
+        "first_name": "Brandon",
+        "last_name": "Thomas",
+        "email": "bthomas@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10021: {
+        "title": "Receptionist",
+        "first_name": "Nicole",
+        "last_name": "Carter",
+        "email": "ncarter@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10022: {
+        "title": "Software Quality Assurance Analyst",
+        "first_name": "Justin",
+        "last_name": "Lewis",
+        "email": "jlewis@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10023: {
+        "title": "HR Director",
+        "first_name": "Ashely",
+        "last_name": "White",
+        "email": "awhite@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10024: {
+        "title": "IT Administrator",
+        "first_name": "David",
+        "last_name": "Moore",
+        "email": "dmoore@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10025: {
+        "title": "Customer Service Representative",
+        "first_name": "Taylor",
+        "last_name": "Hall",
+        "email": "thall@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10026: {
+        "title": "Software Support Specialist",
+        "first_name": "Victoria",
+        "last_name": "Lopez",
+        "email": "vlopez@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10027: {
+        "title": "HR Consultant ",
+        "first_name": "Kyle",
+        "last_name": "Jackson",
+        "email": "kjackson@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10028: {
+        "title": "IT Coordinator",
+        "first_name": "Christina",
+        "last_name": "Adams",
+        "email": "cadams@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10029: {
+        "title": "Administrative Coordinator",
+        "first_name": "Eric",
+        "last_name": "Hill",
+        "email": "ehill@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10030: {
+        "title": "Software Project Manager",
+        "first_name": "Heather",
+        "last_name": "Baker",
+        "email": "hbaker01@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10031: {
+        "title": "HR Analyst",
+        "first_name": "Patrick",
+        "last_name": "Scott",
+        "email": "pscott@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10032: {
+        "title": "Desk Support Technician",
+        "first_name": "Matthew",
+        "last_name": "Evans",
+        "email": "mevans@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10032: {
+        "title": "Software Implementation Specialist",
+        "first_name": "Laura",
+        "last_name": "Phillips",
+        "email": "lphillips@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10033: {
+        "title": "Software Developer",
+        "first_name": "Jamie",
+        "last_name": "Cooper",
+        "email": "jcooper@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10034: {
+        "title": "IT Helpdesk Technician",
+        "first_name": "Rachel",
+        "last_name": "Kim",
+        "email": "rkim@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10035: {
+        "title": "Desk Worker",
+        "first_name": "Brandon",
+        "last_name": "Turner",
+        "email": "bturner@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10036: {
+        "title": "Software Engineer",
+        "first_name": "Alexis",
+        "last_name": "Martinez",
+        "email": "amartinez01@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10037: {
+        "title": "HR Assistant",
+        "first_name": "Nathan",
+        "last_name": "Walker",
+        "email": "nwalker@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10038: {
+        "title": "IT Support Specialist",
+        "first_name": "Monica",
+        "last_name": "Patel",
+        "email": "mpatel@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10039: {
+        "title": "Administrative Assistant",
+        "first_name": "Ethan",
+        "last_name": "Brooks",
+        "email": "ebrooks@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10040: {
+        "title": "Software Tester",
+        "first_name": "Madison",
+        "last_name": "Price",
+        "email": "mprice@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10041: {
+        "title": "HR Specialist",
+        "first_name": "Jordan",
+        "last_name": "Rodriguez",
+        "email": "jrodriguez@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10042: {
+        "title": "IT Analyst",
+        "first_name": "Vanessa",
+        "last_name": "Lee",
+        "email": "vlee@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10043: {
+        "title": "Office Manager",
+        "first_name": "Dylan",
+        "last_name": "Carter",
+        "email": "dcarter@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10044: {
+        "title": "Software Architect",
+        "first_name": "Haley",
+        "last_name": "King",
+        "email": "hking@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10045: {
+        "title": "HR Generalist",
+        "first_name": "Logan",
+        "last_name": "Perez",
+        "email": "lperez@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10046: {
+        "title": "Helpdesk Support",
+        "first_name": "Morgan",
+        "last_name": "Davis",
+        "email": "mdavis@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10047: {
+        "title": "Data Entry Clerk",
+        "first_name": "Taylor",
+        "last_name": "Nguyen",
+        "email": "tnguyen@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10048: {
+        "title": "Software Development Manager",
+        "first_name": "Cameron",
+        "last_name": "Wright",
+        "email": "cwright@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10049: {
+        "title": "HR Consultant",
+        "first_name": "Sydney",
+        "last_name": "Garcia",
+        "email": "sgarcia@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10050: {
+        "title": "Receptionist",
+        "first_name": "Jordan",
+        "last_name": "Hillary",
+        "email": "jhillary@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10051: {
+        "title": "Software Quality Assurance Analyst",
+        "first_name": "Avery",
+        "last_name": "Martinez",
+        "email": "amartinez02@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10052: {
+        "title": "HR Director",
+        "first_name": "Riley",
+        "last_name": "Thomas",
+        "email": "rthomas@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10053: {
+        "title": "IT Administrator",
+        "first_name": "Dylan",
+        "last_name": "Adams",
+        "email": "dadams@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10054: {
+        "title": "Customer Service Representative",
+        "first_name": "Morgan",
+        "last_name": "White",
+        "email": "mwhite@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10055: {
+        "title": "Software Support Specialist",
+        "first_name": "Jordan",
+        "last_name": "Robinson",
+        "email": "jrobinson@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10056: {
+        "title": "HR Coordinator",
+        "first_name": "Casey",
+        "last_name": "Jackson",
+        "email": "cjackson@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10057: {
+        "title": "IT Coordinator",
+        "first_name": "Skylar",
+        "last_name": "Brown",
+        "email": "sbrown@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10057: {
+        "title": "Administrative Coordinator",
+        "first_name": "Hayden",
+        "last_name": "Hill",
+        "email": "hhill@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10058: {
+        "title": "Software Project Manager",
+        "first_name": "Logan",
+        "last_name": "Baker",
+        "email": "lbaker@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10059: {
+        "title": "HR Analyst",
+        "first_name": "Taylor",
+        "last_name": "Scott",
+        "email": "tscott@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10060: {
+        "title": "IT Manager",
+        "first_name": "Sydney",
+        "last_name": "Rivera",
+        "email": "srivera02@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10061: {
+        "title": "Desk Support Technician",
+        "first_name": "Cameron",
+        "last_name": "Evans",
+        "email": "cevans@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10062: {
+        "title": "Software Implementation Specialist",
+        "first_name": "Avery",
+        "last_name": "Phillips",
+        "email": "aphillips@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10063: {
+        "title": "Software Developer",
+        "first_name": "Dakota",
+        "last_name": "Cooper",
+        "email": "dcooper@pyt.com",
+        "location": "Mukwonago"
+    },
+
+    10064: {
+        "title": "HR Manager",
+        "first_name": "Morgan",
+        "last_name": "Morgan",
+        "email": "mmorgan@pyt01.com",
+        "location": "Mukwonago"
+    },
+
+    10065: {
+        "title": "Manager",
+        "first_name": "Marry",
+        "last_name": "Morgan",
+        "email": "mmorgan@pyt02.com",
+        "location": "Mukwonago"
+    },
+    
+    10066: {
+        "title": "Software Project Director",
+        "first_name": "Haily",
+        "last_name": "Baker",
+        "email": "hbaker02@pyt.com",
+        "location": "Mukwonago"
+    },
+    # Add more users here
+}
+
+# Makes sure emails are unique
+#for user_id, user_info in users.items():
+#    email = user_info["email"]
+#    for user_id2, user_info2 in users.items():
+#        if user_id != user_id2 and email == user_info2["email"]:
+#            print(f"Email {email} is not unique")
+#            break
+
+# I already know that the emails are unique so I commented out the code above
+# I also tested this code segment and it works
+
+
+
+
+# Accessing user information using user id
+#ask the user for the user id
+#_id = int(input("Enter a user ID: "))
+#user_info = users[user_id]
+#print("User ID:", user_id)
+#print("Title:", user_info["title"])
+#print("First Name:", user_info["first_name"])
+#print("Last Name:", user_info["last_name"])
+#print("Email:", user_info["email"])
+#print("Location:", user_info["location"])
+
+
+# Accessing user information using title
+#ask the user for the staff member's title
+#staff_title = input("Enter a staff members title: ")
+#for user_id, user_info in users.items():
+#    if user_info["title"] == staff_title:
+#        print("User ID:", user_id)
+#        print("Title:", user_info["title"])
+#        print("First Name:", user_info["first_name"])
+#        print("Last Name:", user_info["last_name"])
+#        print("Email:", user_info["email"])
+#        print("Location:", user_info["location"])
+#        print()
+
+# Accessing user information using first name and last name
+# ask the user for the staff member's first name
+#staff_first_name = input("Enter a staff members first name: ")
+#staff_last_name = input("Enter a staff members last name: ")
+#staff_first_name = staff_first_name.capitalize()
+#staff_last_name = staff_last_name.capitalize()
+
+#for user_id, user_info in users.items():
+#    if user_info["first_name"] == staff_first_name and user_info["last_name"] == staff_last_name:
+#        print("User ID:", user_id)
+#        print("Title:", user_info["title"])
+#        print("First Name:", user_info["first_name"])
+#        print("Last Name:", user_info["last_name"])
+#        print("Email:", user_info["email"])
+#        print("Location:", user_info["location"])
+#        print()
+
+# Accessing user information using email
+# ask the user for the staff member's email
+#staff_email = input("Enter a staff members email: ")
+#staff_email = staff_email.lower()
+
+#for user_id, user_info in users.items():
+#    if user_info["email"] == staff_email:
+#        print("User ID:", user_id)
+#        print("Title:", user_info["title"])
+#        print("First Name:", user_info["first_name"])
+#        print("Last Name:", user_info["last_name"])
+#        print("Email:", user_info["email"])
+#        print("Location:", user_info["location"])
+#        print()
